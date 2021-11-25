884e03e10d9a6655ba4656cd9e4315095d8add18
Nina Cressoni <ninacressoni@gmail.com>
2021-11-25 19:01:10 -0300

    Setting up app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+  // const [data, setData] >>> (currentState, changethecurrentState)
+  // useState hook is going to return the current state of the 
+  //component and the function that will let us update that value
+
+  // useEffect hook will allow us to code the graql api once the dom or object model of
+  // the component is completely rendered
+
 function App() {
+  const [data, setData] = useState( { videos: [] } );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Videos<h1/>
+      <ul>
+        <li></li>
+      </ul>
+
     </div>
   );
 }
